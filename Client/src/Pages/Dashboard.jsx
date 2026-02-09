25ab4e727758794c45f869e231a181b70415325c
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-09 16:32:25 +0100

    Ai added

--- Client/src/Pages/Dashboard.jsx
+++ Client/src/Pages/Dashboard.jsx
@@ -1,37 +1,54 @@
-import { LucideArrowRight, LucideChevronRight } from 'lucide-react'
+import { LucideArrowRight, LucideChevronRight, LucideHistory, LucideRecycle } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className='w-full pr-10'>
       {/* First section */}
-      <h1 className='text-3xl text'>Welcome back, Stalwart!</h1>
-      <p className='text-gray-400'>What will you like to learn today?</p>
+      <h1 className='text-3xl text pt-10'>Welcome back, Stalwart!</h1>
+      <p className='text-gray-400 pt-2'>What will you like to learn today?</p>
 
       {/* Second Section */}
-      <div className='border p-4 rounded-lg border-gray-300'>
+      <div className='border p-4 rounded-lg mt-4 border-gray-300'>
         <div className='flex gap-2 w-full'>
-          <input type="text" placeholder='e.g Learn javascript' className='border p-2 rounded-md border-gray-300'/>
+          <input type="text" placeholder='e.g Learn javascript' className='border p-2 rounded-md border-gray-300 w-[80%]' />
           <button className='bg-[#4f46e5] text-white p-2.5 rounded-md font-bold'>Generate Course</button>
         </div>
-        <p className='text-gray-400 text-sm'>AI would create a personlaized leaening path git any topic you want</p>
+        <p className='text-gray-400 text-sm pt-4'>AI would create a personlaized leaening path git any topic you want</p>
       </div>
       
       {/* Third Section */}
       <div className='bg-[#4f46e5] p-10 w-full mt-4 rounded-lg'>
         <h1 className='text-gray-300 pb-4'>CONTINUE LEARNING</h1>
-        <h1 className='text-gray-300 text-3xl pb-4'>How to bake banana bread</h1>
+        <h1 className='text-gray-300 text-3xl pb-4'>Javascript for absolute beginners</h1>
         <p className='text-sm text-gray-300 pb-2'>Ai-generated Course</p>
-        <progress max="100" value="45" className='border rounded-4xl'>45%</progress>
+        <progress max="100" value="45" className='border rounded-4xl w-full'>45%</progress>
         <p className='text-sm text-gray-300 pt-2'>Lesson 3 of 6 || 50% complete</p>
         <button className='flex text-[#4f46e5] p-2.5 bg-white rounded-lg mt-4'>Continue Lesson <LucideChevronRight/></button>
       </div>
 
       {/* Fourth Section */}
-      <div>
-        <div>
-          <h1>8</h1>
-          <p className=''>Topics Generated</p>
+      <div className='flex gap-3 mt-4'>
+        <div className='bg-gray-100 flex flex-col w-full rounded-md p-4 border border-gray-300'>
+          <h1 className='text-3xl font-bold text-center'>8</h1>
+          <p className='text-sm text-center text-gray-500'>Topics Generated</p>
+        </div>
+           <div className='bg-gray-100 flex flex-col w-full rounded-md p-4 border border-gray-300'>
+          <h1 className='text-3xl font-bold text-center'>5</h1>
+          <p className='text-sm text-center text-gray-500'>In Progress</p>
+        </div>
+           <div className='bg-gray-100 flex flex-col w-full rounded-md p-4 border border-gray-300'>
+          <h1 className='text-3xl font-bold text-center'>3</h1>
+          <p className='text-sm text-center text-gray-500'>Completed</p>
+        </div>
+      </div>
+
+      {/* Fifth Section */}
+      <div className='bg-gray-100 p-4 mt-4 rounded-md border border-gray-300'>
+        <h1 className='flex gap-2 font-bold pb-2'>Your Recent Searches <LucideHistory/></h1>
+        <div className='flex flex-col gap-2'>
+          <p className='p-2 border rounded-md border-gray-300 text-gray-500'>Javascript for beginners</p>
+          <p className='p-2 border rounded-md border-gray-300 text-gray-500'>Understanding system design in Javascript</p>
         </div>
       </div>
 
