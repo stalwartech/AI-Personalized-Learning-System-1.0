aec3f63dc3416abee8d668936373a47253ca0c3b
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-10 09:38:31 +0100

    Settings UI to be updated

--- Client/src/Pages/Settings.jsx
+++ Client/src/Pages/Settings.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className='w-full pt-10 pr-10'>
+      {/* Heading */}
+      <h1 className='text-3xl font-bold'>Settings</h1>
+      {/* profile Information */}
+      <div className='w-full border rounded-lg border-gray-400'>
+        <h1>Profile Information</h1>
+        <div className='flex justify-between'>
+          <h1>Full Name</h1>
+          <input type="text" className='border rounded-lg' />
+        </div>
+      </div>
+    </div>
   )
 }
 
