3cca30b783b1388a9e0a314474af4278b2c2e562
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-10 09:48:34 +0100

    Settings is been updated

--- Client/src/Pages/Settings.jsx
+++ Client/src/Pages/Settings.jsx
@@ -6,10 +6,10 @@ const Settings = () => {
       {/* Heading */}
       <h1 className='text-3xl font-bold'>Settings</h1>
       {/* profile Information */}
-      <div className='w-full border rounded-lg border-gray-400'>
-        <h1>Profile Information</h1>
-        <div className='flex justify-between'>
-          <h1>Full Name</h1>
+      <div className='p-6 w-full border rounded-lg border-gray-400 mt-4'>
+        <h1 className='text-2xl font-bold'>Profile Information</h1>
+        <div className='flex justify-between mt-6'>
+          <h1 className='text-xl'>Full Name</h1>
           <input type="text" className='border rounded-lg' />
         </div>
       </div>
