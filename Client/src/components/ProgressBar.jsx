d126d74643e293b8b78fd339930a41ec667d25a5
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-11 23:57:50 +0100

    Nothing much

--- Client/src/components/ProgressBar.jsx
+++ Client/src/components/ProgressBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProgressBar = ({value}) => {
   return (
-    <div className="w-full bg-gray-300 rounded-full h-3 overflow-hidden">
+    <div className="w-full bg-gray-300 rounded-full h-2 overflow-hidden">
       <div
         className="bg-[#4f46e5] h-3 rounded-full transition-all duration-500 ease-in-out"
         style={{ width: `${value}%` }}
