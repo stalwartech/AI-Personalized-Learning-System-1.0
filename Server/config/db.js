b7884f26e9693b53fe7cf47338cbb6aee3c79967
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-14 09:18:39 +0100

    Mongodb cconnected

---
+++ Server/config/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const URI = process.env.URI
+const db = mongoose.connect(URI, console.log("DB connected"));
+
+
+module.exports = db
--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,9 @@ const app = express();
 app.use(express.json());
 const env = require('dotenv').config();
 
+// All file imports here 
+const {db} = require('./config/db')
+
 const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => {
