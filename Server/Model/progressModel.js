d2ea18d52cefc54d8ed1bba73d8754cb4df1965e
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-17 19:03:38 +0100

    nothing much

--- Server/Model/progressModel.js
+++ Server/Model/progressModel.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 // Daily activity sub schema (Embedded)
 const dailyActivitySchema = new mongoose.Schema({
     date: {type: Date, required: true},
-    timeSpent: {type: Number, required: true},
-    lessonsCompleted: {type: Number, required: true},
-})
+    timeSpent: {type: Number, required: true}, // minutes
+    lessonsCompleted: {type: Number, default: 0},
+},
+{_id: false}
+)
