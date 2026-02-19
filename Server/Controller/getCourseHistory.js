99d9239f37669892fdde4c04773c8095fa806e9c
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-19 23:28:32 +0100

    Nothing much

--- Server/Controller/getCourseHistory.js
+++ Server/Controller/getCourseHistory.js
@@ -9,8 +9,8 @@ const getCourseHistory = async (req, res) => {
     const [courses, total] = await Promise.all([
       Course.find(filter)
         .sort({ updatedAt: -1 }) // sort by updated date
-        .limit(parseInt(limit))
-        .skip(parseInt(skip))
+        .limit(parseInt(limit)) // limit the number of results
+        .skip(parseInt(skip)) // skip the number of results
         .select('-lessons.content -lessons.notes'), // skip heavy fields in list view
       Course.countDocuments(filter)
     ]);
