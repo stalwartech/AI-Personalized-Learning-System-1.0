e9f3937f62b4f57072eadf12e21b21cda5cc450c
Stalwartech <abdulsalamakintaro@gmail.com>
2026-03-04 09:22:43 +0100

    Added settings o work succesfully

--- Server/Route/settingsRoute.js
+++ Server/Route/settingsRoute.js
@@ -21,12 +21,12 @@ const profileValidation = [
 const passwordValidation = [
   body('currentPassword').notEmpty().withMessage('Current password is required'),
   body('newPassword').isLength({ min: 8 }).withMessage('New password must be at least 8 characters'),
-  body('confirmPassword').custom((value, { req }) => {
-    if (value !== req.body.newPassword) {
-      throw new Error('Passwords do not match');
-    }
-    return true;
-  })
+  // body('confirmPassword').custom((value, { req }) => {
+  //   if (value !== req.body.newPassword) {
+  //     throw new Error('Passwords do not match');
+  //   }
+  //   return true;
+  // })
 ];
 
 const preferencesValidation = [
