1fc83da6e063b3e7ae67658d08bdcbb5705382d9
Stalwartech <Abdulsalamakintaro@gmail.com>
2026-04-29 23:18:52 +0100

    idk

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -106,9 +106,7 @@ const LoginForm = () => {
                         errors.password ? 'border-red-500' : 'border-gray-300'
                       }`}
                     />
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3">
               <Eye className="w-5 h-5 text-gray-500" />
-              </div>
             </div>
       
             {errors.password && (
