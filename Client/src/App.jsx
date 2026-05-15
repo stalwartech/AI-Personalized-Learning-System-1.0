05a8b27e54e3d89b706cd7f3d5529cdca31249af
Stalwartech <Abdulsalamakintaro@gmail.com>
2026-05-15 23:10:24 +0100

    Added protected route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import Sidebar from './components/Sidebar'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import Learn from './Pages/Learn'
 import Progress from './Pages/Progress'
@@ -9,6 +9,37 @@ import Settings from './Pages/Settings'
 import History from './Pages/History'
 import Premium from './Pages/Premium'
 import Quiz from './Pages/Quiz'
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token')
+
+  if (!token) {
+    return <Navigate to="/login" replace />
+  }
+
+  return children
+}
+
+const ProtectedLayout = () => (
+  <div className="min-h-screen bg-slate-50 lg:flex">
+    <Sidebar />
+    <div className="min-w-0 flex-1 pb-24 lg:pb-0">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/Learn" element={<Learn />} />
+        <Route path="/Learn/:courseId" element={<Learn />} />
+        <Route path="/Progress" element={<Progress />} />
+        <Route path="/History" element={<History />} />
+        <Route path="/Premium" element={<Premium />} />
+        <Route path="/Quiz" element={<Quiz />} />
+        <Route path="/Quiz/:courseId" element={<Quiz />} />
+        <Route path="/Settings" element={<Settings />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  </div>
+)
+
 function App() {
 
 
@@ -20,22 +51,10 @@ function App() {
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
 
-    <Route path="/*" element={<div className="min-h-screen bg-slate-50 lg:flex">
-          <Sidebar />
-          <div className="min-w-0 flex-1 pb-24 lg:pb-0">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/Learn" element={<Learn />} />
-              <Route path="/Learn/:courseId" element={<Learn />} />
-              <Route path="/Progress" element={<Progress />} />
-              <Route path="/History" element={<History />} />
-              <Route path="/Premium" element={<Premium />} />
-              <Route path="/Quiz" element={<Quiz />} />
-              <Route path="/Quiz/:courseId" element={<Quiz />} />
-              <Route path="/Settings" element={<Settings />} />
-            </Routes>
-          </div>
-        </div>
+    <Route path="/*" element={
+        <ProtectedRoute>
+          <ProtectedLayout />
+        </ProtectedRoute>
       }
     />
 
